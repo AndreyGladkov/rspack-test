687222e8a633f886978c6899facc050729280992
Eugene Zaikov <e.zaykov@bi.zone>
2024-09-23 15:42:59 +0500

    test 2

---
+++ dist2/568.js
@@ -0,0 +1,20 @@
+"use strict";
+(self.webpackChunkrspack_test = self.webpackChunkrspack_test || []).push([
+  ["568"],
+  {
+    58: function (e, s, t) {
+      t.r(s),
+        t.d(s, {
+          Test: function () {
+            return n;
+          },
+        });
+      var c = t(250);
+      let n = () =>
+        (0, c.jsx)("div", {
+          children: (0, c.jsx)("h1", { children: "Test1" }),
+        });
+    },
+  },
+]);
+//# sourceMappingURL=568.js.map
--- src/Test.tsx
+++ src/Test.tsx
@@ -1,7 +1,7 @@
 export const Test = () => {
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Test1</h1>
     </div>
   );
 };
